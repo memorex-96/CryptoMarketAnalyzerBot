f9ed45c8d577d85c230553fe35bd87bcc9257aa5
memorex-96 <tigercrowley@gmail.com>
2025-08-16 22:23:00 -0400

    changed func name to lookup, added comments for plans

--- CrypBot/main.py
+++ CrypBot/main.py
@@ -21,20 +21,27 @@ bot = commands.Bot(command_prefix= '/', intents=intents)
 async def on_ready(): 
     print(f"We are ready to go, in {bot.user.name}!")  # modify 
 
+
+# Daily Market Data Announcement 
+
+
+
 # commands
 @bot.command() 
 async def cmd_help(ctx): 
     help_text = ( 
         "Welcome to the Crypto Market Analyzer Bot!\n", 
         "Here are the commands you can use: \n", 
-        "/cmd_help - Show this help message\n"
-        "/curr_price <coin_name> - Get the current price of a cryptocurrency in USD\n"   
+        "/cmd_help - Show this help message\n",
+        "/lookup <coin_name> - Get the current price of a cryptocurrency in USD\n",
+        "To be added:\n", 
+        "/project <coin_name> <amount> <timeframe> - Get coin investment details over a specified timeframe\n",   
     )
     await ctx.reply("".join(help_text))
 
  
-@bot.command() 
-async def curr_price(ctx, coin:str): 
+@bot.command()          # need to fix, give parse JSON for coin profile, read docs  
+async def lookup(ctx, coin:str): 
     try:
         data = cg.get_price(ids=coin, vs_currencies='usd')
         if coin.lower() in data: 
@@ -45,6 +52,9 @@ async def curr_price(ctx, coin:str):
     except Exception as e: 
         await ctx.reply(f"Error fetching coin data.")
         print(e) 
+
+# /project command 
+# access another file for mathmatical calculations 
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG) 
 
